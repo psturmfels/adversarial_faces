b9be1836f4eaabe84d405c2083868b428721762b
Ivan Evtimov <ie5@partyparrot>
2020-04-02 18:35:29 -0700

    run attacks script now also does eps=0.04

--- run_attacks.sh
+++ run_attacks.sh
@@ -1,4 +1,5 @@
 python run_adversarial_attacks.py --epsilon 0.02
+python run_adversarial_attacks.py --epsilon 0.04
 python run_adversarial_attacks.py --epsilon 0.06
 python run_adversarial_attacks.py --epsilon 0.08
 python run_adversarial_attacks.py --epsilon 0.1
