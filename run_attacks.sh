619191f128f3a74a3760a29896b9cac884fbbc53
Ivan Evtimov <ie5@partyparrot>
2020-04-15 09:07:05 -0700

    ready to rerun community naive random attack

--- run_attacks.sh
+++ run_attacks.sh
@@ -1,4 +1,4 @@
-python run_adversarial_attacks.py --epsilon 0.02 --visible_devices 0 --attack_type community_sample_gaussian_model
-python run_adversarial_attacks.py --epsilon 0.04 --visible_devices 0 --attack_type community_sample_gaussian_model
-python run_adversarial_attacks.py --epsilon 0.06 --visible_devices 0 --attack_type community_sample_gaussian_model
+python run_adversarial_attacks.py --epsilon 0.02 --visible_devices 0 --attack_type community_naive_random
+python run_adversarial_attacks.py --epsilon 0.04 --visible_devices 0 --attack_type community_naive_random
+python run_adversarial_attacks.py --epsilon 0.06 --visible_devices 0 --attack_type community_naive_random
 
