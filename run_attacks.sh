7487886c6f20d4327596de3a4c0511e705c99f65
Ivan Evtimov <ie5@partyparrot>
2020-06-01 12:36:50 -0700

    just grabbing everything

--- run_attacks.sh
+++ run_attacks.sh
@@ -1,5 +1,13 @@
+#python run_adversarial_attacks.py --epsilon 0.2 --visible_devices 0 --attack_type community_naive_same
+#python run_adversarial_attacks.py --epsilon 0.5 --visible_devices 0 --attack_type community_naive_same
+#python run_adversarial_attacks.py --epsilon 0.7 --visible_devices 0 --attack_type community_naive_same
+python postprocess.py --epsilon 0.2 --format png --attack_type community_naive_same
+python postprocess.py --epsilon 0.5 --format png --attack_type community_naive_same
+python postprocess.py --epsilon 0.7 --format png --attack_type community_naive_same
 
-python run_adversarial_attacks.py --epsilon 0.04 --visible_devices 0 --attack_type community_naive_same
+
+
+#python run_adversarial_attacks.py --epsilon 0.04 --visible_devices 0 --attack_type community_naive_same
 #python run_adversarial_attacks.py --epsilon 0.04 --visible_devices 0 --attack_type community_naive_mean
 #python run_adversarial_attacks.py --epsilon 0.02 --visible_devices 0 --attack_type community_naive_mean
 
--- run_attacks_1.sh
+++ run_attacks_1.sh
@@ -1,3 +1,2 @@
 python run_adversarial_attacks.py --epsilon 0.02 --visible_devices 1 --attack_type community_naive_random_iterated
 python run_adversarial_attacks.py --epsilon 0.04 --visible_devices 1 --attack_type community_naive_random_iterated
-python run_adversarial_attacks.py --epsilon 0.08 --visible_devices 1 --attack_type community_naive_random_iterated
